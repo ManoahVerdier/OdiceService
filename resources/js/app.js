d3f1c20892fe5b7d3def555af15379766dd6ff11
Manoah Verdier <verdier.developpement@gmail.com>
2022-09-23 17:30:06 +0200

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@ import.meta.glob([
   '../fonts/**',
 ]);
 "use strict";
-import '@/js/lib/jquery.min.js';
+import '@/lib/jquery.min.js';
 window.$ = jQuery;
-import '@/js/lib/jquery.nice-select.min.js';
+import '@/lib/jquery.nice-select.min.js';
 
 import 'bootstrap';
 import 'magnific-popup';
