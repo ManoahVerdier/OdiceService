e1b5779a6390eeaa1b06928b9d38cfbf3572ab3b
Manoah Verdier <verdier.developpement@gmail.com>
2022-10-07 17:25:50 +0200

    test

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -126,7 +126,7 @@ class SubscriptionController extends Controller
         $file = ($path.".docx");
         $request = new \Aspose\Words\Model\Requests\ConvertDocumentRequest($file, $format,null);
         $result = $api->ConvertDocument($request);	
-        copy($result->getPathName(),$path.".pdf");
+        copy($result->getPathName(),"OdiceService_ContratNonSigne.pdf");
     }
 
     public function makeSignature(Request $request){
